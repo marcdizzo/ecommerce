1a8ee03fc04d7d06fe90a92c560c7043db418d35
marc dizzo <marcmarkion456@gmail.com>
2023-07-02 16:14:22 +0300

    Updated Api

--- src/services/appApi.js
+++ src/services/appApi.js
@@ -4,7 +4,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const appApi = createApi({
     reducerPath: "appApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://ecommerce-backend-app-api.onrender.com" }),
+    // http://localhost:5000
     endpoints: (builder) => ({
         signup: builder.mutation({
             query: (user) => ({
